0bb3de1a0ade89cc652bfa988b3a50433ddd1d25
Adrien Burgun <adrien.burgun@orange.fr>
2019-05-03 12:19:48 +0200

    :pencil2: Fixing that file

--- lib/easysdl.h
+++ lib/easysdl.h
@@ -19,24 +19,24 @@ typedef struct{
 // Ferme les extensions et la SDL
 void EZ_destruction_fenetre();
 
-//Initialise SDL ses extensions et crée une fenetre
+//Initialise SDL ses extensions et crÃ©e une fenetre
 int EZ_creation_fenetre(char* nom,unsigned int x, unsigned int y);
 
-// Place un point de la couleur donnée
+// Place un point de la couleur donnÃ©e
 int EZ_trace_point(unsigned int x,unsigned int y,unsigned char rouge,unsigned char vert,unsigned char bleu,unsigned char transparence);
 
-// trace un rectangle de la couleur donnée (x et y sont les coordonnées du coin supérieur gauche
+// trace un rectangle de la couleur donnÃ©e (x et y sont les coordonnÃ©es du coin supÃ©rieur gauche
 int EZ_trace_rectangle_plein(unsigned int x,unsigned int y,unsigned int base,unsigned int hauteur,unsigned char rouge,unsigned char vert,unsigned char bleu,unsigned char transparence);
 
-// trace le contour d'un rectangle de la couleur donnée (x et y sont les coordonnées du coin supérieur gauche
+// trace le contour d'un rectangle de la couleur donnÃ©e (x et y sont les coordonnÃ©es du coin supÃ©rieur gauche
 int EZ_trace_rectangle(unsigned int x,unsigned int y,unsigned int base,unsigned int hauteur,unsigned char rouge,unsigned char vert,unsigned char bleu,unsigned char transparence);
 
 
 
-// trace un segment de couleur donné, la fonction peut utiliser antialisaing mais est plus lente
+// trace un segment de couleur donnÃ©, la fonction peut utiliser antialisaing mais est plus lente
 int EZ_trace_segment( int x,int y,int x2, int y2,unsigned char rouge,unsigned char vert,unsigned char bleu,unsigned char transparence);
 
-// trace un triangle de couleur et de sommets donnés
+// trace un triangle de couleur et de sommets donnÃ©s
 int EZ_trace_triangle_plein(int x1,int y1,int x2,int y2,int x3,int y3,unsigned char rouge,unsigned char vert,unsigned char bleu,unsigned char transparence);
 
 // Trace un cercle de centre et rayon la fonction peut utiliser antialisaing mais est plus lente
@@ -48,22 +48,22 @@ int EZ_trace_disque(int x,int y,unsigned int rayon,unsigned char rouge,unsigned 
 // Trace un arc angulair de centre x et y, R1 rayon petit cercle R2 rayon grand cercle angle 1 et angle 2 puis couleur.
 int EZ_trace_secteur(int x,int y,int r1,int r2,double a1,double a2,unsigned char rouge,unsigned char vert,unsigned char bleu,unsigned char transparence);
 
-// trace une image de nom donné à la position donné
+// trace une image de nom donnÃ© Ã© la position donnÃ©
 int EZ_trace_image(char* nom,unsigned int x,unsigned int y);
 
-// trace une image (x et y sont les coordonnées du coin supérieur gauche avec une trasparence 0 transparent 255 opaque
+// trace une image (x et y sont les coordonnÃ©es du coin supÃ©rieur gauche avec une trasparence 0 transparent 255 opaque
 int EZ_trace_image_transparence(char* nom,unsigned int x,unsigned int y,unsigned char transparence);
 
-// Stocke une image de nom donné à la position donné
+// Stocke une image de nom donnÃ© Ã© la position donnÃ©
 void EZ_stocke_image(int position,char* nom);
 
-// Trace l'image stocké a la position donné.
+// Trace l'image stockÃ© a la position donnÃ©.
 void EZ_trace_image_stocke(int position,int x,int y);
 
-// La même mais avec la transparence
+// La mÃ©me mais avec la transparence
 void EZ_trace_image_stocke_transparence(int position,int x,int y,unsigned char transparence);
 
-// Mise à jour graphique
+// Mise Ã© jour graphique
 void EZ_mise_a_jour();
 
 
@@ -72,47 +72,47 @@ void EZ_mise_a_jour();
 // Active l'aliasing
 void EZ_antialiasing_on();
 
-// Désactive l'aliasing
+// DÃ©sactive l'aliasing
 void EZ_antialiasing_off();
 
-// Test si l'aliasing est activée 1 si oui
+// Test si l'aliasing est activÃ©e 1 si oui
 int EZ_test_aliasing();
 
 /// Fonction de texte ///
 
-// trace un texte (x et y sont les coordonnées du coin supérieur gauche
+// trace un texte (x et y sont les coordonnÃ©es du coin supÃ©rieur gauche
 int EZ_trace_texte(char* txt,char* nom_police,int taille, unsigned int x,unsigned int y,unsigned char rouge,unsigned char vert,unsigned char bleu,unsigned char transparence);
 
-// trace un texte lissé sur une couleur de fond (x et y sont les coordonnées du coin supérieur gauche
+// trace un texte lissÃ© sur une couleur de fond (x et y sont les coordonnÃ©es du coin supÃ©rieur gauche
 int EZ_trace_texte_shaded(char* txt,char* nom_police,int taille, unsigned int x,unsigned int y,unsigned char rouge,unsigned char vert,unsigned char bleu,unsigned char rougef,unsigned char vertf,unsigned char bleuf);
 
-// trace un texte (x et y sont les coordonnées du coin supérieur gauche
+// trace un texte (x et y sont les coordonnÃ©es du coin supÃ©rieur gauche
 int EZ_trace_texte_droite(char* txt,char* nom_police,int taille, unsigned int x,unsigned int y,unsigned char rouge,unsigned char vert,unsigned char bleu,unsigned char transparence);
 
-// trace un texte lissé sur une couleur de fond (x et y sont les coordonnées du coin supérieur gauche
+// trace un texte lissÃ© sur une couleur de fond (x et y sont les coordonnÃ©es du coin supÃ©rieur gauche
 int EZ_trace_texte_shaded_droite(char* txt,char* nom_police,int taille, unsigned int x,unsigned int y,unsigned char rouge,unsigned char vert,unsigned char bleu,unsigned char rougef,unsigned char vertf,unsigned char bleuf);
 
 
-/// Fonction de traitement sur l'image sauvegardé ///
+/// Fonction de traitement sur l'image sauvegardÃ© ///
 
 
  //Sauvegarde l'ecran
 int EZ_sauvegarde_ecran();
 
-// Récupére la couleur d'un pixel de la sauvegarde
+// RÃ©cupÃ©re la couleur d'un pixel de la sauvegarde
 int EZ_recupere_pixel(unsigned x,unsigned y,unsigned char *rouge,unsigned char *vert,unsigned char *bleu);
 
-// Restore l'ecran sauvegardé préalablement
+// Restore l'ecran sauvegardÃ© prÃ©alablement
 int EZ_restoration_ecran();
 
-// Recupére les dimensions de l'image
+// RecupÃ©re les dimensions de l'image
 void EZ_recupere_dimension(int *x,int* y);
 
 
 ///Fonction musicale ///
 
 
-// Charge le son "nom" et le place à la position donnee
+// Charge le son "nom" et le place Ã© la position donnee
 void EZ_stocke_son(int position,char* nom);
 
 // joue le son un nombre nb de fois.
@@ -135,7 +135,7 @@ int EZ_recupere_evenement();
 // Donne le type d'un evenement avec une limite de temps
 int EZ_recupere_evenement_temps_limite(int temps);
 
-// Donne le type de l'événement dans le buffer (non bloquant)
+// Donne le type de l'Ã©vÃ©nement dans le buffer (non bloquant)
 int EZ_recupere_evenement_continu();
 
 //donne la position de la souris en x
@@ -144,16 +144,16 @@ int EZ_souris_x();
 //donne la position de la souris en y
 int EZ_souris_y();
 
-// Donne la touche appuyée
+// Donne la touche appuyÃ©e
 int EZ_touche();
 
 // Attendre un certain temps
 void EZ_attendre(unsigned int temps);
 
-//Attendre temps qu'une touche ou exit n'est pas enfoncée
+//Attendre temps qu'une touche ou exit n'est pas enfoncÃ©e
 int EZ_attendre_touche();
 
-//Attendre temps qu'une touche ou un click ou exit n'est pas enfoncée si Exit alors retourn 0;
+//Attendre temps qu'une touche ou un click ou exit n'est pas enfoncÃ©e si Exit alors retourn 0;
 int EZ_attendre_action();
 
 // Vide le buffer evenement
@@ -194,7 +194,7 @@ void EZ_sauvegarde_EZ_image_P3(EZ_image* image,char* nom);
 // Sauvegarde une image en format BMP
 void EZ_sauvegarde_EZ_image_BMP(EZ_image* image,char* nom);
 
-// trace une EZ_image (x et y sont les coordonnées du coin supérieur gauche avec une trasparence 0 transparent 255 opaque
+// trace une EZ_image (x et y sont les coordonnÃ©es du coin supÃ©rieur gauche avec une trasparence 0 transparent 255 opaque
 int EZ_trace_EZ_image_transparence(EZ_image* image,unsigned int x,unsigned int y,unsigned char transparence);
 
 // colorie un pixel avec la transparence t
@@ -203,10 +203,10 @@ void EZ_colorie_rvb_image_transparence(EZ_image* image,int x,int y,unsigned char
 // retourne la couleur en RGBA
 void EZ_recupere_rvb_image_transparence(EZ_image* image,int x,int y,unsigned char* r,unsigned char* v,unsigned char* b,unsigned char* t);
 
-// Comme la fonction EZ_trace_image sauf que l'on donne l'image déjà chargée
+// Comme la fonction EZ_trace_image sauf que l'on donne l'image dÃ©jÃ© chargÃ©e
 int EZ_trace_EZ_image(EZ_image* image,int x,int y);
 
-// retourne l'image transformée avec une rotation et un zoom
+// retourne l'image transformÃ©e avec une rotation et un zoom
 EZ_image* EZ_transforme_EZ_image(EZ_image* image,double angle,double zoom);
 
 #endif // EASYSDL_H_INCLUDED
