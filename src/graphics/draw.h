7adc58ef59bdfb8fa290298f9cd60a1e34ca5ed8
Adrien Burgun <adrien.burgun@orange.fr>
2019-06-07 08:25:42 +0200

    :bulb: Commenting src/graphics/draw.h

--- src/graphics/draw.h
+++ src/graphics/draw.h
@@ -16,6 +16,16 @@
 
 #define HEIGHT_ADJUST 0.21
 
+/*!
+ * Fait le rendu du monde en perspective à la première personne.
+ * @param game Instance de Game, contenant les options du jeu
+ * @param world Instance de World, d'où sera lu les murs, la position du joueur et les salles
+ * @param layer (unused) Index du niveau dans World
+ * @param fov Field of vision, valeur en radians du secteur d'angle du champ de vision
+ * @param length Distance maximale visible
+ * @param bg Couleur du fond
+ * @param mist_length Valeur représentant l'intensité du brouillard; des valeurs plus grandes indiquent un brouillard moins intense
+ */
 void draw(
         const Game* game,
         const World* world,
@@ -23,8 +33,21 @@ void draw(
         const Color* bg, double mist_length
 );
 
+
+/*!
+ * Calcule la hauteur qu'un mur devrait avoir
+ * @param distance La distance par rapport à la caméra du mur
+ * @param angle L'angle entre le milieu du champ de vision et l'intersection avec le mur
+ * @param height Hauteur de la fenêtre
+ * @parma width Largeur de la fenêtre
+ * @return Hauteur du mur
+ */
 double calc_height(double distance, double angle, int height, int width);
 
+/*!
+ * Retourne si la texture est complexe (checkerboard ou image) ou non
+ * @return `true` si oui, `false` si non
+ */
 bool is_complex_texture(void* tex);
 
 #endif //FICTIONAL_COUSCOUS_RAYTRACING_H
